973dd0ad39f7cb4c1de1f61fbd0923278f73fd70
janowskipio <janowskipio@gmail.com>
2015-05-11 12:20:18 +0200

    Update menu.py

--- menu.py
+++ menu.py
@@ -7,18 +7,25 @@ if not pygame.font.get_init():
 
 
 class Menu(object):
-    curr_position = 0
+    ''' Simple class designed for drawing menus.
+        The simplest way to initialize this menu in your program is to call these three lines:
+        menu = Menu(['field1', 'field2', 'field3', 'Exit'])
+        menu.init(x)  # x variable should contain screen surface, e.g. called by pygame.display.set_mode()
+        menu.draw()
+    '''
+    curr_position = 0  # variable used for getting current highlighted field
     font_size = 60
     font_style = 'bauhaus93'
     ground_colour = (0, 0, 0)
     text_colour = (255, 255, 255)
-    curr_selection_colour = (60, 200, 60)
+    curr_selection_colour = (60, 200, 60)  # highlight colour
 
     def __init__(self, text_list):
         self.text_list = text_list
         self.fields_num = len(text_list)
 
     def set_colors(self, ground_color, text_colour, curr_selection):
+        # allows alternative change of colour sets
         self.ground_colour = ground_color
         self.text_list = text_colour
         self.curr_selection_colour = curr_selection
@@ -27,21 +34,24 @@ class Menu(object):
         self.font_size = font_size
 
     def init(self, surface):
+        # method for initializing font and getting surface
+        # while calling this method, one should pass the variable with screen surface
         self.font = pygame.font.SysFont(self.font_style, self.font_size)
         self.surface = surface
 
     def draw(self, move=0):
+        # responsible for checking which field should be highlighted, getting current fields positions required for drawing
+        # and drawing all of the menu on screen
         self.surface.fill(self.ground_colour)
         if self.curr_position + move in range(self.fields_num):
             self.curr_position += move
         screen_size = self.surface.get_size()
-        menu_height = self.font_size * self.fields_num + self.font_size * (self.fields_num - 1)
-        center = [screen_size[0] / 2, (screen_size[1] - menu_height) / 2 + self.font_size]
+        menu_height = self.font.size(self.text_list[-1])[1] * self.fields_num
+        center = [screen_size[0] / 2, (screen_size[1] - menu_height) / 2]
         pos = [0, center[1]]
         for i in range(self.fields_num):
             pos[0] = center[0] - self.font.size(self.text_list[i])[0] / 2
             if i == self.curr_position:
-                # text = self.font.render(self.text_list[i], 1, self.text_colour, self.curr_selection_colour)
                 text = self.font.render(self.text_list[i], 1, self.curr_selection_colour)
                 self.surface.blit(text, pos)
             else:
@@ -51,6 +61,8 @@ class Menu(object):
         pygame.display.update()
 
     def start(self):
+        # main menu loop, you can exit by pressing ESC keyboard button, by choosing EXIT field
+        # or just by clicking eXit (close) button on a window
         while True:
             for event in pygame.event.get():
                 if event.type == QUIT:
